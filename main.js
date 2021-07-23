d1fe49168a319ed1f4001c1e6415b8a34765bb0d
ano201 <ahmu689.com>
2021-07-23 21:52:15 +0600

    fixed

--- main.js
+++ main.js
@@ -22,14 +22,12 @@ function clearAll(newNumber) {
 //---mode button
 const dotBtn = document.getElementById("dot");
 const main = document.getElementById("main");
-dotBtn.addEventListener("click", function() {
-  if (dotBtn.style.left != 0) {
-    dotBtn.style.backgroundColor = "white";
-    dotBtn.style.left = 0;
-    main.style.backgroundColor = "#1A1A21";
-  } else {
+function mode() {
+  if (dotBtn.style.left == 0) {
     dotBtn.style.backgroundColor = "red";
     dotBtn.style.left = "65px";
     main.style.backgroundColor = "white";
+  } else {
+    location.reload(true);
   }
-})
+}
