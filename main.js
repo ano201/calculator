73a9dcbb5fe799c2f9b9fc54d706f8a9b0e40408
ano201 <ahmu689.com>
2021-07-23 21:40:43 +0600

    mode button added

--- main.js
+++ main.js
@@ -18,3 +18,18 @@ function clearAll(newNumber) {
 }
 
 //----time and mode button
+
+//---mode button
+const dotBtn = document.getElementById("dot");
+const main = document.getElementById("main");
+dotBtn.addEventListener("click", function() {
+  if (dotBtn.style.left != 0) {
+    dotBtn.style.backgroundColor = "white";
+    dotBtn.style.left = 0;
+    main.style.backgroundColor = "#1A1A21";
+  } else {
+    dotBtn.style.backgroundColor = "red";
+    dotBtn.style.left = "65px";
+    main.style.backgroundColor = "white";
+  }
+})
